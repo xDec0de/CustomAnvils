73ff3beb7c1a3cbb5874ab85ed8705af7134283b
xDec0de_ <xdec0deyt@gmail.com>
2024-04-01 01:52:31 +0200

    Basic plugin setup

---
+++ src/main/java/me/xdec0de/customanvils/CustomAnvils.java
@@ -0,0 +1,16 @@
+package me.xdec0de.customanvils;
+
+import net.codersky.mcutils.MCPlugin;
+
+public class CustomAnvils extends MCPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+
+	@Override
+	public void onDisable() {
+		
+	}
+}
